26c778e126b3b9317a623bd45aec7068fc11ec3c
Andrej Lapin <lapin.andrej1@gmail.com>
2026-01-31 18:08:10 +0200

    cool sounds

--- scripts/office_layout.gd
+++ scripts/office_layout.gd
@@ -2,16 +2,19 @@ class_name OfficeLayout extends Node3D
 
 
 @onready var animation_player: AnimationPlayer = $office_layout/Lift/AnimationPlayer
+@onready var elevator_jingle: AudioStreamPlayer3D = $ElevatorJingle
+@onready var elevator_door_open: AudioStreamPlayer3D = $ElevatorDoorOpen
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# TODO: play some kind of lift arrive sound
+	await get_tree().create_timer(0.2).timeout
+	elevator_jingle.play()
 	await get_tree().create_timer(0.5).timeout
-	# TODO: elevator doors open
+	elevator_door_open.play()
 	animation_player.play("lift_door")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+	#pass
