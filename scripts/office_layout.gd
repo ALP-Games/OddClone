317787413b9c5d13cc4dc2c3b437ad999ec8b296
Andrej Lapin <lapin.andrej1@gmail.com>
2026-02-01 12:24:10 +0200

    delay controls

--- scripts/office_layout.gd
+++ scripts/office_layout.gd
@@ -5,12 +5,14 @@ class_name OfficeLayout extends Node3D
 @onready var elevator_jingle: AudioStreamPlayer3D = $ElevatorJingle
 @onready var elevator_door_open: AudioStreamPlayer3D = $ElevatorDoorOpen
 
+@export var arrival_delay: float = 0.2
+@export var delay_after_jingle: float = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(arrival_delay).timeout
 	elevator_jingle.play()
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(delay_after_jingle).timeout
 	elevator_door_open.play()
 	animation_player.play("lift_door")
 
